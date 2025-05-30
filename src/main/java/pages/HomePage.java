979f483e1ea7b80a07a051486c2a05ff669b4b6e
bu134 <chanman134431@gmail.com>
2025-05-31 01:26:12 +0530

    Added new feature file step

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -11,6 +11,7 @@ public class HomePage {
     private By searchIcon = By.cssSelector("button[data-target='#search-box']");
     private By searchInput = By.id("head-search-box");
     private By shareArticleTitle = By.cssSelector("#main-content .post__social--top.post__social--has-title > h2");
+    private By linkedLink = By.xpath("//div[@class='post__social post__social--v2 post__social--top post__social--has-title']//a[contains(@href,'linkedin')]");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -29,5 +30,13 @@ public class HomePage {
             System.out.println("Exception while pausing : " + e.getMessage());
         }
         return driver.findElement(shareArticleTitle).getText();
+    }
+
+    public Boolean verifySocialMediaIconsAndLinks(){
+        return driver.findElement(linkedLink).isEnabled();
+    }
+
+    public String fetchSocialMediaLabel(){
+        return driver.findElement(linkedLink).getAttribute("aria-label");
     }
 }
